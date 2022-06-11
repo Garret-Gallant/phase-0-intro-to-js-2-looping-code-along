f8cac9aa5f976302aa98795cea24cf0aabf5697f
Garret Gallant <gallanttechnological@gmail.com>
2022-06-11 17:38:24 -0600

    Project Complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const array = [
+    "Guadalupe",
+    "Ollie", 
+    "Aki"
+]
+
+
+function writeCards(array){
+    console.log(array);
+    const newArray = [];
+
+     for (let i = 0; i < array.length; i++) {
+        console.log(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+        newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }
+    return newArray;
+}
+writeCards(array);
+
+
+//Working Function, DON'T TOUCH!
+function countDown(num){
+    for (let counter = num; num >= 0; num--) {
+        console.log(num);
+    }
+}
+
